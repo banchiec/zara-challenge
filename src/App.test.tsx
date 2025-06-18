63b9dd00369fa90e8699fdb48e382a759d66eb18
Ivan Espinoza Condori <ivanec@ext.inditex.com>
2025-06-18 08:56:02 +0200

    feature/service-get-list-phones

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,7 @@ import App from './App';
 describe('App', () => {
   it('renders without crashing', () => {
     render(<App />);
-    expect(screen.getByText(/Edit/i));
+    const hmrText = screen.getByText('Loading...');
+    expect(hmrText).toBeDefined();
   });
 });
