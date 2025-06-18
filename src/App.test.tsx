520fd00618658dea6c088bda0f9a2893cccd7fd9
Ivan Espinoza Condori <ivanec@ext.inditex.com>
2025-06-18 08:48:57 +0200

    chro/project-setup create ci github workflow

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,6 @@ import App from './App';
 describe('App', () => {
   it('renders without crashing', () => {
     render(<App />);
-    expect(screen.getByText(/Edit/i));
+    expect(screen.getByText(/HMR/i));
   });
 });
