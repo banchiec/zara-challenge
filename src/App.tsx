bd007f273488c811dc0baaec9edab88f39d1bfcb
Ivan Espinoza Condori <ivanec@ext.inditex.com>
2025-06-18 13:25:32 +0200

    feature/service-get-by-id-phone add service

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
-import './App.css';
+import { useEffect } from 'react';
+import { useListPhones } from './hooks';
 
 function App() {
+  const { phones, loading, error } = useListPhones();
+  console.log(phones);
+
+  useEffect(() => {
+    if (error) {
+      console.error('Error fetching phones:', error);
+    }
+  }, [error]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <div className="card">
@@ -8,6 +22,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
