7926045b1e3d7c525281ab03a36573c09aeb1030
Iván Espinoza <64547613+banchiec@users.noreply.github.com>
2025-06-30 09:07:13 +0200

    Update cart-item-list

--- src/views/cart-phone-view/components/cart-item-list/cart-item-list.tsx
+++ src/views/cart-phone-view/components/cart-item-list/cart-item-list.tsx
@@ -61,43 +61,6 @@ const CartItemsList = () => {
         totalPrice={totalPrice}
         handleContinue={handleContinue}
       />
-      {/** 
-       * 
-      <div className="zara-challenge-cart-items-actions">
-        {!isCartEmpty ? (
-          <div className="zara-challenge-cart-items-summary">
-            <div className="zara-challenge-cart-items-total">
-              <span>{CART_PHONE.total}</span>
-              <span>
-                {totalPrice.toFixed(2)} {CURRENCIES.EUR}
-              </span>
-            </div>
-            <div className="zara-challenge-cart-items-buttons">
-              <div className="zara-challenge-cart-items-buttons">
-                <button
-                  className="zara-challenge-cart-items-continue"
-                  onClick={handleContinue}
-                >
-                  {CART_PHONE.shopping_continue}
-                </button>
-              </div>
-              <button className="zara-challenge-cart-items-pay">PAY</button>
-            </div>
-          </div>
-        ) : (
-          <div className="zara-challenge-cart-items-summary">
-            <div className="zara-challenge-cart-items-buttons">
-              <button
-                className="zara-challenge-cart-items-continue"
-                onClick={handleContinue}
-              >
-                {CART_PHONE.shopping_continue}
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
-      */}
     </div>
   );
 };
